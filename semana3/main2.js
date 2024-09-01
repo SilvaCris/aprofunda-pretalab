885f203539b069cdf110f2e2bd709a380695363a
Crislaine <88508656+SilvaCris@users.noreply.github.com>
2024-09-01 00:17:42 -0300

    Delete semana3 directory

--- semana3/main2.js
+++
@@ -1,8 +0,0 @@
-const PostFactory = require('./factory2')
-
-
-const article = PostFactory.createPost('artigo', 'Introdução ao Node.js', 'Node.js é um ambiente...')
-console.log(article.getDetails())
-
-const news = PostFactory.createPost('noticia', 'lançamento do bun', ' BUN é um ambiente...')
-console.log(news.getDetails())
